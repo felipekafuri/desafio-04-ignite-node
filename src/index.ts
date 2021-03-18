9d41879289904f7ff53c0f52988977c722ffb2a3
felipekafuri <felipekafuri@discente.ufg.br>
2021-03-18 10:42:55 -0300

    desafio 05

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import * as swagger from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerJson from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+app.use("/api-docs", swagger.serve, swagger.setup(swaggerJson));
 
 app.use("/users", usersRoutes);
 
