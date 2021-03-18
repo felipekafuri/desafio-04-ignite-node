8c2f100b2fb11a463ba937f172a29e62d194ff98
felipekafuri <felipekafuri@discente.ufg.br>
2021-03-17 22:06:08 -0300

    desafio 04

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,15 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.params;
+
+      const users = this.listAllUsersUseCase.execute({ user_id });
+
+      return response.send(200).json(users);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,16 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const isUserAdmin = this.usersRepository.findById(user_id);
+
+    if (!isUserAdmin.admin) {
+      throw new Error("User is not admin");
+    }
+
+    const users = this.usersRepository.list();
+    console.log(users);
+
+    return users;
   }
 }
 
